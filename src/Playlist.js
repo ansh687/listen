d559f9986354c39dcdf43d54f93f26e7378299e7
Anshu Das <ansh@Anshus-MacBook-Pro.local>
2019-08-25 16:32:53 -0400

    temp blue test

--- src/Playlist.js
+++ src/Playlist.js
@@ -4,10 +4,10 @@ import './Playlist.css'
 const Playlist = () => {
 	return (
 		<div className='playlistContainer'> 
-			<iframe src="https://open.spotify.com/embed/playlist/5Ta18q9fdwMjgNzbPTvAGD" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" className='iframeElement'></iframe>
 		</div> 			
 		); 
 }
 
 export default Playlist 
 
+//<iframe src="https://open.spotify.com/embed/playlist/5Ta18q9fdwMjgNzbPTvAGD" allowtransparency="true" allow="encrypted-media" className='iframeElement'></iframe>
