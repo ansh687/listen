1b435ce30bab38703e2882cb9d6d2b376ad1a805
Anshu Das <ansh@Anshus-MacBook-Pro.local>
2019-08-25 18:44:28 -0400

    fixed mobile viewport issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import Playlist from './Playlist.js'
+import Playlist from './Playlist.js';
 import './App.css';
 
 function App() {
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   return (
-    <div className='Window'> 
-      <div className='Container'>
-          <img src={require('./london.png')} className='Image' /> 
-      </div>
-      <div className='Playlist'>
-              <Playlist className='playlistElement' /> 
-      </div> 
-      <div className='Logo'> 
-        hhear.me
-      </div> 
-    </div> 
+        <div className='Window'> 
+          <div className='Container'>
+              <img src={require('./london.png')} className='Image' /> 
+          </div>
+          <div className='Playlist'>
+                  <Playlist className='playlistElement' /> 
+          </div> 
+          <div className='Logo'> 
+            hhear.me
+          </div> 
+        </div> 
   );
 }
 
