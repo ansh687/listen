e975a30cac1fdddbf19d249b237b18d5bc974a7a
Anshu Das <ansh@Anshus-MacBook-Pro.local>
2019-08-25 18:55:48 -0400

    updated to top 5 playlist

--- src/Playlist.js
+++ src/Playlist.js
@@ -4,10 +4,9 @@ import './Playlist.css'
 const Playlist = () => {
 	return (
 		<div className='playlistContainer'> 
-			<iframe src="https://open.spotify.com/embed/playlist/5Ta18q9fdwMjgNzbPTvAGD" allowtransparency="true" allow="encrypted-media" className='iframeElement'></iframe>
+			<iframe src="https://open.spotify.com/embed/playlist/0oQxQo2Qng0Jii9yHMa57q" allowtransparency="true" allow="encrypted-media" className='iframeElement'></iframe>
 		</div> 			
 		); 
 }
 
 export default Playlist 
-
